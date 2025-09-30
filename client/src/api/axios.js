16ba5f39be276b47c5b1e67a6693e16f7f6b5eae
Raziya-023 <raziaa0023@gmail.com>
2025-10-01 01:09:19 +0530

    Update production API base URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://food-feasto-api.onrender.com',
+    baseURL: 'https://food-feasto-api.onrender.com/api/v1',
     withCredentials: true
 });
 
